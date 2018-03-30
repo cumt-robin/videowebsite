f92c0a05ee3535a5b77eadedb7d5b5255d4f8a8c
Tusi <1148121254@qq.com>
2018-03-30 09:31:08 +0800

    【提交人】Robin
    【提交信息】1.增加css预处理器Sass；增加替换脚本

---
+++ replaceScss.js
@@ -0,0 +1,55 @@
+var fs = require("fs");
+ 
+readDir(__dirname + '/src/app/home');
+ 
+function readDir(path) {  
+    fs.readdir(path,function(err, dirContent) {
+        if(!dirContent) {
+           return;
+        }
+        dirContent.forEach(function(ele) {
+            fs.stat(path+"/"+ele, function(err, info){
+                if (info.isDirectory()) {
+                    readDir(path+"/" + ele);
+                } else if (info.isFile()) {  
+                    if (getFileType(ele) === 'scss') {
+                        console.log("file: " + ele);
+                        fs.readFile(path+"/" + ele, function(err, data){
+                            if(err){
+                                return console.log(err);
+                            }
+                            let newContent = replaceContent(data.toString());
+                            if (newContent !== data.toString()) {
+                                fs.writeFile(path+"/" + ele, newContent, function(err) {
+                                    if (err) {
+                                        return console.log(err);
+                                    }
+                                }); 
+                            }
+                        });
+                    }
+                }
+            });
+        });        
+    });
+}
+ 
+function getFileType(fileName) {
+    var splitName = fileName.split('.');
+    let splitLength = splitName.length;
+    var type = splitName[splitLength - 1];
+    return type;
+}
+ 
+function replaceContent(content) {
+    content = pxToRem(content);
+    return content;
+}
+ 
+function pxToRem(content) {
+    return content.replace(/([:\s])(-?\d+.?\d?)px/g, '$1' + 'rem(' + '$2' + ')');
+}
+ 
+function remToPx(content) {
+    return content.replace(/rem\((-?\d+.?\d?)\)/g, '$1' + 'px');
+}
